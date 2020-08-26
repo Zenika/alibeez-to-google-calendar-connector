0a46e5d104aeeff16a3c00c1fb24f6d7b599da59
jonathan VARELA <jonathan.varela@zenika.com>
2020-08-26 11:29:55 +0200

    simplify persistence (no need to check if user is valid, we trust google and alibeez)

--- src/users/persistence.js
+++ src/users/persistence.js
@@ -1,26 +1,13 @@
 import * as fs from "fs";
 
-export const persistence = {
-  upsert: async function upsert(user) {
-    const {isValidUser, error} = persistence.validateUser(user);
+export async function userUpsert(user) {
+  await fs.promises.mkdir("users", { recursive: true });
+  await fs.promises.writeFile(
+    `users/data/${user.alibeezId}.json`,
+    JSON.stringify(user)
+  );
 
-    if (!isValidUser) {
-      return error;
-    }
-
-    await fs.promises.mkdir("users", { recursive: true });
-    await fs.promises.writeFile(
-      `users/data/${user.alibeezId}.json`,
-      JSON.stringify(user)
-    );
-
-    return true;
-  },
-
-  validateUser() {
-    return { isValidUser: true };
-    //todo
-  }
+  return true;
 }
 
 
