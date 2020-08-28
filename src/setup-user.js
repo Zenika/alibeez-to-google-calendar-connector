ec6c1555d0ceded01b4a44fa2c59c8579902eb87
Hugo Wood <hugo.wood@zenika.com>
2020-08-28 16:15:05 +0200

    merge user mapper inside setup user

--- src/setup-user.js
+++ src/setup-user.js
@@ -1,7 +1,6 @@
 import { parseJwtClaims } from "./utils/jwt.js";
 import { exchangeCodeForTokens, claimsMatch } from "./google-oauth-client.js";
 import { getUserByUsername } from "./alibeez-client.js";
-import { mapUser } from "./users/user-mapper.js";
 import { userService } from "./users/user-service.js";
 
 export async function setupUser(code) {
@@ -11,13 +10,13 @@ export async function setupUser(code) {
     return null;
   }
   const request = await getUserByUsername(claims.email);
-  const alibeezId = request.result[0].uuid;
-  const accessToken = tokens.access_token;
-  const user = mapUser({
-    ...claims,
-    alibeezId: alibeezId,
-    accessToken: accessToken,
+  const user = {
+    email: claims.email,
+    alibeezId: request.result[0].uuid,
+    googleId: claims.sub,
+    accessTokenExpiration: new Date(claims.exp * 1000).toISOString(),
+    accessToken: tokens.access_token,
     refreshToken: tokens.refresh_token,
-  });
+  };
   return await userService.upsert(user);
 }
