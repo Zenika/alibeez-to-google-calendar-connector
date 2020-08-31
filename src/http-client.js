e3dd2a499d1fdce70ebde3cb724217efc1ef2ce8
Hugo Wood <hugo.wood@zenika.com>
2020-08-31 14:44:02 +0200

    rename http request options to native options

--- src/http-client.js
+++ src/http-client.js
@@ -6,16 +6,16 @@ import * as https from "https";
  * @param {{ url: string | URL, body?: string } & https.RequestOptions} options
  * @returns {Promise<http.IncomingMessage>}
  */
-export function request({ url, body, ...options }) {
+export function request({ url, body, ...nativeOptions }) {
   const effectiveOptions = body
     ? {
-        ...options,
+        ...nativeOptions,
         headers: {
-          ...options.headers,
+          ...nativeOptions.headers,
           "Content-Length": Buffer.byteLength(body),
         },
       }
-    : options;
+    : nativeOptions;
   return new Promise((resolve, reject) => {
     const req = https.request(url, effectiveOptions, resolve);
     req.on("error", reject);
