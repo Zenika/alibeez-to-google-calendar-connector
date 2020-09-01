86e3d69bbd5d152a0f82e3051cce91d74a80498d
Hugo Wood <hugo.wood@zenika.com>
2020-09-01 17:00:06 +0200

    🐛 fix crash when removing an event that used to exist
    In this case Calendar responds with 410, but we expect 404 when the event is not there so the 410 is treated as an error.

--- src/google-calendar-actions.js
+++ src/google-calendar-actions.js
@@ -14,7 +14,10 @@ export async function upsert(calendarId, eventId, eventBody, accessToken) {
   try {
     return await updateEvent(calendarId, eventId, eventBody, accessToken);
   } catch (err) {
-    if (err instanceof http.IncomingMessage && err.statusCode === 404) {
+    if (
+      err instanceof http.IncomingMessage &&
+      [404, 410].includes(err.statusCode)
+    ) {
       return await insertEvent(
         calendarId,
         { id: eventId, ...eventBody },
@@ -30,7 +33,10 @@ export async function removeIfExists(calendarId, eventId, accessToken) {
   try {
     return await removeEvent(calendarId, eventId, accessToken);
   } catch (err) {
-    if (err instanceof http.IncomingMessage && err.statusCode === 404) {
+    if (
+      err instanceof http.IncomingMessage &&
+      [404, 410].includes(err.statusCode)
+    ) {
       return err;
     } else {
       throw err;
