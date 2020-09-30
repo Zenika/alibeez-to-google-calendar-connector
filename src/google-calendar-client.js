d75a1c5e50d9021c9cd9d8f126277756c12230cf
Hugo Wood <hugo.wood@zenika.com>
2020-10-01 00:50:28 +0200

    🛠 flag as test mode
    See #12

--- src/google-calendar-client.js
+++ src/google-calendar-client.js
@@ -1,5 +1,14 @@
 import { request, parseBodyAsJson } from "./http-client.js";
 
+const FLAG_CALENDAR_EVENTS_AS_TESTS =
+  process.env.FLAG_CALENDAR_EVENTS_AS_TESTS === "true";
+
+if (FLAG_CALENDAR_EVENTS_AS_TESTS) {
+  console.warn(
+    `WARN: FLAG_CALENDAR_EVENTS_AS_TESTS is set to 'true'! Events written to Google Calendar will be flagged as tests.`
+  );
+}
+
 export async function getCalendar(calendarId, accessToken) {
   const response = await request({
     url: `https://www.googleapis.com/calendar/v3/calendars/${calendarId}`,
@@ -13,6 +22,9 @@ export async function getCalendar(calendarId, accessToken) {
 }
 
 export async function insertEvent(calendarId, eventBody, accessToken) {
+  if (FLAG_CALENDAR_EVENTS_AS_TESTS) {
+    [eventBody] = flagAsTest(eventBody);
+  }
   const response = await request({
     url: `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
     method: "POST",
@@ -27,6 +39,9 @@ export async function insertEvent(calendarId, eventBody, accessToken) {
 }
 
 export async function updateEvent(calendarId, eventId, eventBody, accessToken) {
+  if (FLAG_CALENDAR_EVENTS_AS_TESTS) {
+    [eventBody, eventId] = flagAsTest(eventBody, eventId);
+  }
   const response = await request({
     url: `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/${eventId}`,
     method: "PUT",
@@ -50,4 +65,20 @@ export async function removeEvent(calendarId, eventId, accessToken) {
   });
   const responseBody = await parseBodyAsJson(response);
   return responseBody;
+}
+
+function flagAsTest(eventBody, eventId) {
+  const testEventId = eventId ? `test${eventId}` : eventId;
+  const testEventBody = {
+    ...eventBody,
+    summary: `[TEST 🙈] ${eventBody?.summary ?? ""}`,
+    extendedProperties: {
+      ...eventBody?.extendedProperties,
+      shared: {
+        ...eventBody?.extendedProperties?.shared,
+        test: true,
+      },
+    },
+  };
+  return [testEventBody, testEventId];
 }
