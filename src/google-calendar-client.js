3bef99613437342bf8846623f43c04cbaa068924
Hugo Wood <hugo.wood@zenika.com>
2020-10-07 17:08:24 +0200

    🛠 flag as test mode (#15)
    See #12

--- src/google-calendar-client.js
+++ src/google-calendar-client.js
@@ -3,7 +3,18 @@ import { request, parseBodyAsJson } from "./http-client.js";
 const DRY_RUN = process.env.DRY_RUN === "true";
 
 if (DRY_RUN) {
-  console.warn("WARN: DRY_RUN is set to 'true'! Nothing will be written to Google Calendar.")
+  console.warn(
+    "WARN: DRY_RUN is set to 'true'! Nothing will be written to Google Calendar."
+  );
+}
+
+const FLAG_CALENDAR_EVENTS_AS_TESTS =
+  process.env.FLAG_CALENDAR_EVENTS_AS_TESTS === "true";
+
+if (FLAG_CALENDAR_EVENTS_AS_TESTS) {
+  console.warn(
+    `WARN: FLAG_CALENDAR_EVENTS_AS_TESTS is set to 'true'! Events written to Google Calendar will be flagged as tests.`
+  );
 }
 
 export async function getCalendar(calendarId, accessToken) {
@@ -22,6 +33,9 @@ export async function insertEvent(calendarId, eventBody, accessToken) {
   if (DRY_RUN) {
     return;
   }
+  if (FLAG_CALENDAR_EVENTS_AS_TESTS) {
+    [eventBody] = flagAsTest(eventBody);
+  }
   const response = await request({
     url: `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
     method: "POST",
@@ -38,6 +52,9 @@ export async function insertEvent(calendarId, eventBody, accessToken) {
 export async function updateEvent(calendarId, eventId, eventBody, accessToken) {
   if (DRY_RUN) {
     return;
+  }
+  if (FLAG_CALENDAR_EVENTS_AS_TESTS) {
+    [eventBody, eventId] = flagAsTest(eventBody, eventId);
   }
   const response = await request({
     url: `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/${eventId}`,
@@ -65,4 +82,20 @@ export async function removeEvent(calendarId, eventId, accessToken) {
   });
   const responseBody = await parseBodyAsJson(response);
   return responseBody;
+}
+
+function flagAsTest(eventBody, eventId) {
+  const testEventId = eventId ? `test${eventId}` : eventId;
+  const testEventBody = {
+    ...eventBody,
+    summary: `[TEST 🙈] ${eventBody?.summary ?? ""}`,
+    extendedProperties: {
+      ...eventBody?.extendedProperties,
+      shared: {
+        ...eventBody?.extendedProperties?.shared,
+        test: true,
+      },
+    },
+  };
+  return [testEventBody, testEventId];
 }
